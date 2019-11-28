5c57d64de4283765812573cb04580275482fc694
Marcelo Teixeira <celocesar30@hotmail.com>
2019-11-27 21:27:14 -0300

    Projeto criado

---
+++ src/main/java/com/marcelo/system/System32Application.java
@@ -0,0 +1,13 @@
+package com.marcelo.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class System32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(System32Application.class, args);
+	}
+
+}
