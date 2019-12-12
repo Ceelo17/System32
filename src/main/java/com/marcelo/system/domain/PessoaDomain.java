44b5df5f5d1c249df2e7857ab0384048213328bb
Marcelo <celocesar17@gmail.com>
2019-12-12 16:05:54 -0300

    Estilo página cadastrarPessoa completo

--- src/main/java/com/marcelo/system/domain/PessoaDomain.java
+++ src/main/java/com/marcelo/system/domain/PessoaDomain.java
@@ -2,6 +2,7 @@ package com.marcelo.system.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -11,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class PessoaDomain implements Serializable {
@@ -20,6 +23,10 @@ public class PessoaDomain implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	private String nome;
+	private String email;
+
+	@Temporal(TemporalType.DATE)
+	private Date dtNascimento;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pessoa", fetch = FetchType.EAGER)
 	private List<TarefaDomain> tarefas = new ArrayList<>();
@@ -28,10 +35,12 @@ public class PessoaDomain implements Serializable {
 		super();
 	}
 
-	public PessoaDomain(Long id, String nome, List<TarefaDomain> tarefas) {
+	public PessoaDomain(Long id, String nome, String email, Date dtNascimento, List<TarefaDomain> tarefas) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.email = email;
+		this.dtNascimento = dtNascimento;
 		this.tarefas = tarefas;
 	}
 
@@ -51,6 +60,22 @@ public class PessoaDomain implements Serializable {
 		this.nome = nome;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
 	public List<TarefaDomain> getTarefas() {
 		return tarefas;
 	}
@@ -63,8 +88,11 @@ public class PessoaDomain implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((dtNascimento == null) ? 0 : dtNascimento.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((tarefas == null) ? 0 : tarefas.hashCode());
 		return result;
 	}
 
@@ -77,6 +105,16 @@ public class PessoaDomain implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		PessoaDomain other = (PessoaDomain) obj;
+		if (dtNascimento == null) {
+			if (other.dtNascimento != null)
+				return false;
+		} else if (!dtNascimento.equals(other.dtNascimento))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -86,6 +124,11 @@ public class PessoaDomain implements Serializable {
 			if (other.nome != null)
 				return false;
 		} else if (!nome.equals(other.nome))
+			return false;
+		if (tarefas == null) {
+			if (other.tarefas != null)
+				return false;
+		} else if (!tarefas.equals(other.tarefas))
 			return false;
 		return true;
 	}
